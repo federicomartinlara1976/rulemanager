af1afaead9af1d0eeb317c209e857dc5826a8a15
federicomartinlara1976@gmail.com <federicomartinlara1976@gmail.com>
2025-02-15 19:05:24 +0100

    Configuración correcta de caché

--- src/main/java/net/bounceme/chronos/rulemanager/config/CacheConfig.java
+++ src/main/java/net/bounceme/chronos/rulemanager/config/CacheConfig.java
@@ -1,54 +1,68 @@
 package net.bounceme.chronos.rulemanager.config;
 
+import javax.annotation.PreDestroy;
+
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.commons.marshall.ProtoStreamMarshaller;
 import org.infinispan.spring.remote.provider.SpringRemoteCacheManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import net.bounceme.chronos.rulemanager.support.SimpleKeyMarshaller;
 
 @Configuration
 @EnableCaching
+@Slf4j
 public class CacheConfig {
-	
+
 	@Value("${spring.cache.server}")
 	private String cacheServer;
-	
+
 	@Value("${spring.cache.port}")
 	private Integer cachePort;
-	
+
 	@Value("${infinispan.remote.auth-username}")
 	private String authUserName;
-	
+
 	@Value("${infinispan.remote.auth-password}")
 	private String authPassword;
-	
+
 	@Value("${infinispan.remote.auth-realm}")
 	private String authRealm;
-	
+
 	@Value("${infinispan.remote.sasl-mechanism}")
 	private String authMechanism;
+	
+	RemoteCacheManager remoteCacheManager;
 
 	@Bean
-	public SpringRemoteCacheManager cacheManager() {
+	SpringRemoteCacheManager cacheManager() {
+		log.info("Iniciando caché");
+		
 		// Configurar el RemoteCacheManager con los detalles de conexión
 		ConfigurationBuilder builder = new ConfigurationBuilder();
-		builder.addServer().host(cacheServer) 
-				.port(cachePort); 
+		builder.addServer().host(cacheServer).port(cachePort);
 
-		builder.security().authentication().username(authUserName) 
-				.password(authPassword) 
-				.realm(authRealm) 
-				.saslMechanism(authMechanism); 
+		builder.addServer().host(cacheServer).port(cachePort).security().authentication().username(authUserName)
+				.password(authPassword).realm(authRealm).saslMechanism(authMechanism).socketTimeout(5000)
+				.connectionTimeout(5000);
 
-		// Configurar el marshaller para utilizar JSON
-        builder.marshaller(new SimpleKeyMarshaller());
+		// Usar un marshaller más robusto si es necesario
+		builder.marshaller(new ProtoStreamMarshaller());
 		
-		RemoteCacheManager remoteCacheManager = new RemoteCacheManager(builder.build());
+		remoteCacheManager = new RemoteCacheManager(builder.build());
 
 		return new SpringRemoteCacheManager(remoteCacheManager);
 	}
+	
+	@PreDestroy
+    public void cleanup() {
+        // Cerrar el RemoteCacheManager cuando la aplicación se detenga
+		log.info("Parando caché");
+        remoteCacheManager.stop();
+    }
 }
